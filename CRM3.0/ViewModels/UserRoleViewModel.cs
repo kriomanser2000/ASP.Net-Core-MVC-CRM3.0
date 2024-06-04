55aaa41a0a767d70e31f2cc63fbab00fcb3a4368
kriomanser2000 <kuzmenkasana05@gmail.com>
2024-06-04 15:27:05 +0300

    created all ViewModels

---
+++ CRM3.0/ViewModels/UserRoleViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CRM3._0.ViewModels
+{
+	public class UserRoleViewModel
+	{
+		public int RoleID { get; set; }
+		[Required]
+		public string RoleName { get; set; }
+		[Required]
+		public int RoleStatus { get; set; }
+	}
+}
