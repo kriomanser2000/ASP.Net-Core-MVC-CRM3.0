81acb25e35e3150700c531d028cf4b6ef6efe4e8
kriomanser2000 <kuzmenkasana05@gmail.com>
2024-06-04 22:03:05 +0300

    added all scripts

---
+++ CRM3.0/Views/DefaultView.cs
@@ -0,0 +1,26 @@
+﻿using CRM3._0.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRM3._0.Views
+{
+    public abstract class DefaultView<T> : WebViewPage<T>
+    {
+        private static ApplicationContext _db = new ApplicationContext();
+
+        protected override void InitializePage()
+        {
+            SetViewBagDefaultProperties();
+            base.InitializePage();
+        }
+
+        private void SetViewBagDefaultProperties()
+        {
+            ViewBag.Users = null;
+            ViewBag.UserRole = Helpers.AccountHelper.GetUserRoleByUsername(ref _db, User.Identity.Name);
+        }
+    }
+}
